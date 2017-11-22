af785907f00460d1c6b0536ee7784dca59fb1a82
MoreiraJorge <moreirajorge_17@outlook.pt>
2017-11-22 18:36:20 +0000

    EX9 pronto e 8 em falta

---
+++ Ficha6/Exercicio9/Utils.h
@@ -0,0 +1,15 @@
+/* 
+ * File:   Utils.h
+ * Author: Jorge Moreira
+ * Ficha6 - Exercicio9
+ * Created on 22 de Novembro de 2017, 18:27
+ */
+
+#ifndef UTILS_H
+#define UTILS_H
+
+void clean_buffer();
+int lerString(char *string, int max);
+
+#endif /* UTILS_H */
+
---
+++ Ficha6/Exercicio9/build/Debug/Cygwin-Windows/Exercicio9.o.d
@@ -0,0 +1,3 @@
+build/Debug/Cygwin-Windows/Exercicio9.o: Exercicio9.c Utils.h
+
+Utils.h:

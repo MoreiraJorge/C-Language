96ef30da31d610e1ea6db8db6c96f2cbaf8fd814
MoreiraJorge <moreirajorge_17@outlook.pt>
2017-11-24 16:12:20 +0000

    Ex6 inacabado

--- Ficha7/Exercicio6/Utils.c
+++ Ficha7/Exercicio6/Utils.c
@@ -28,65 +28,65 @@ int pedirtamanho() {
     return linhas, colunas;
 }
 
-void pedirmatrizA(int matriz[][colunas]){
+void pedirmatrizA(int matriz[][colunas]) {
     int i, j;
-    
+
     puts("Introduza numeros na matriz A");
-    for (i=0; i < linhas; ++i){
-        for (j = 0; j < colunas; ++j){
-          scanf("%d", &matriz[i][j]);  
+    for (i = 0; i < linhas; ++i) {
+        for (j = 0; j < colunas; ++j) {
+            scanf("%d", &matriz[i][j]);
         }
     }
 }
 
-void pedirmatrizB(int matriz[][colunas]){
+void pedirmatrizB(int matriz[][colunas]) {
     int i, j;
-    
+
     puts("Introduza numeros na matriz B");
-    for (i=0; i < linhas; ++i){
-        for (j = 0; j < colunas; ++j){
-          scanf("%d", &matriz[i][j]);  
+    for (i = 0; i < linhas; ++i) {
+        for (j = 0; j < colunas; ++j) {
+            scanf("%d", &matriz[i][j]);
         }
     }
 }
 
-void printmatrizA(int matriz[][colunas]){
+void printmatrizA(int matriz[][colunas]) {
     int i, j;
-    
+
     puts("Matriz A");
-    for (i=0; i < linhas; ++i){
-        for (j = 0; j < colunas; ++j){
-          printf(" %d", matriz[i][j]);  
+    for (i = 0; i < linhas; ++i) {
+        for (j = 0; j < colunas; ++j) {
+            printf(" %d", matriz[i][j]);
         }
         puts(" ");
     }
 }
 
-void printmatrizB(int matriz[][colunas]){
+void printmatrizB(int matriz[][colunas]) {
     int i, j;
-    
+
     puts("Matriz B");
-    for (i=0; i < linhas; ++i){
-        for (j = 0; j < colunas; ++j){
-          printf(" %d", matriz[i][j]);  
+    for (i = 0; i < linhas; ++i) {
+        for (j = 0; j < colunas; ++j) {
+            printf(" %d", matriz[i][j]);
         }
         puts(" ");
     }
 }
 
-void matrizcFINAL(int matrizA[][colunas], int matrizB[][colunas]){
+void matrizcFINAL(int matrizA[][colunas], int matrizB[][colunas]) {
     int i, j;
     int matrizC[linhas][colunas];
-    
-    for(i = 0; i < linhas; ++i){
-        for( j = 0; j < colunas; ++j){
+
+    for (i = 0; i < linhas; ++i) {
+        for (j = 0; j < colunas; ++j) {
             matrizC[i][j] = matrizA[i][j] * matrizB[i][j];
         }
     }
-    
+
     puts("Matriz C");
-    for(i = 0; i < linhas; ++i){
-        for( j = 0; j < colunas; ++j){
+    for (i = 0; i < linhas; ++i) {
+        for (j = 0; j < colunas; ++j) {
             printf(" %d", matrizC[i][j]);
         }
         puts(" ");
